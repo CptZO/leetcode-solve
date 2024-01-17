e1bd3cd46ceb0767bc6e23c10d5f8e1a1712db5e
Ojas <ojasvideepak08@gmail.com>
2024-01-17 20:03:36 +0530

    Code for QOFTD 17-01-24

---
+++ Array/1207-Unique Number of Occurrences.py
@@ -0,0 +1,42 @@
+# Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+
+# Example 1:
+
+# Input: arr = [1,2,2,1,1,3]
+# Output: true
+# Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+# Example 2:
+
+# Input: arr = [1,2]
+# Output: false
+# Example 3:
+
+# Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# Output: true
+
+
+# Constraints:
+
+# 1 <= arr.length <= 1000
+# -1000 <= arr[i] <= 1000
+
+
+# Code
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        hashmap = {}
+        for i in arr:
+            if i in hashmap:
+                hashmap[i] += 1
+            else:
+                hashmap[i] = 1
+        lennum = len(hashmap)
+        checklen = len(set(hashmap.values()))
+        if lennum == checklen:
+            return True
+        else:
+            return False
+
+
+# Link - https://leetcode.com/problems/unique-number-of-occurrences/?envType=daily-question&envId=2024-01-17
